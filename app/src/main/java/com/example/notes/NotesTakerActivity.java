cc16065ad4e3c15c8207a8dbe8daf8f1faa9a8b2
Yaros <Jaroslpa@yandex.ru>
2023-03-02 14:35:50 +0300

    main 13

--- app/src/main/java/com/example/notes/NotesTakerActivity.java
+++ app/src/main/java/com/example/notes/NotesTakerActivity.java
@@ -2,9 +2,16 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class NotesTakerActivity extends AppCompatActivity {
@@ -21,5 +28,33 @@ public class NotesTakerActivity extends AppCompatActivity {
         editText_title = findViewById(R.id.editText_title);
         editText_notes = findViewById(R.id.editText_notes);
         imageViewSave = findViewById(R.id.imageViewSave);
+
+        imageViewSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = editText_title.getText().toString();
+                String desc = editText_notes.getText().toString();
+
+                if(desc.isEmpty()){
+                    Toast.makeText(NotesTakerActivity.this, "Вы не ввели текст", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SimpleDateFormat formatter = new SimpleDateFormat("d MMM yyyy");
+                Date date = new Date();
+
+                notes = new Notes();
+                notes.setTitle(title);
+                notes.setNotes(desc);
+                notes.setDate(formatter.format(date));
+
+                Intent intent = new Intent();
+                intent.putExtra("notes", notes);
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
+
+
     }
 }
